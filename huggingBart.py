e222db0f10baa3bb8abecbb8d59783f93a574da6
boxman42 <94643623+boxman42@users.noreply.github.com>
2024-01-15 19:25:57 -0500

    Hugging Face Bart Model framework

---
+++ huggingBart.py
@@ -0,0 +1,20 @@
+from transformers import AutoTokenizer, AutoModel
+import numpy as np
+
+class BartModel:
+    def __init__(self, huggingFaceModel:str) -> None:
+        self.tokenizer = AutoTokenizer.from_pretrained(huggingFaceModel)
+        self.model = AutoModel.from_pretrained(huggingFaceModel)
+    
+    def getTokens(self, text:str):
+        return self.tokenizer(text)
+    
+    def getEmbeddings(self, text:str):
+        tokens = self.tokenizer(text, return_tensors='pt')
+        # Forward pass through the model
+        outputs = self.model(**tokens)
+        # Extract embeddings from the output
+        word_embeddings = outputs.last_hidden_state
+        # If you want embeddings for all tokens:
+        all_token_embeddings = word_embeddings.detach().numpy()
+        return all_token_embeddings
